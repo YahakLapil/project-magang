f13000b4780535807d23786bb022a1066342ee20
YahakLapil <yahaklapil2004@gmail.com>
2024-10-09 13:51:11 +0700

    menambahkan beberapa fitur

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const express = require("express")
 const mysql = require("mysql")
 const bcrypt = require("bcrypt")
 const BodyParser = require("body-parser")
+const multer = require("multer")
+const moment = require("moment")
 const app = express()
+const upload = multer({ dest: "./uploads/" })
 
 // use express dan bodyparser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(BodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use('/uploads', express.static('uploads'))
+
+
+//membuat library moment tersedia di file edit
+app.locals.moment = require('moment')
 
 // set template engine
 app.set("view engine", "ejs")
@@ -40,6 +48,10 @@ db.connect((err) => {
             db.query(sqlUser , (err, userResults) => {
               if (err) throw err
                 const userResult = JSON.parse(JSON.stringify(userResults))
+                userResult.forEach((user) => {
+                  user.mulai = moment(user.mulai).format("YYYY-MM-DD")
+                  user.akhir = moment(user.akhir).format("YYYY-MM-DD")
+                })
                 console.log("hasil data -> ", userResult, akunResult)
                 res.render("data-mhs", { akun: akunResult, users: userResult, title: "DAFTAR MAHASISWA MAGANG" })
             })
@@ -51,25 +63,69 @@ db.connect((err) => {
     });
 
     // Insert database
-    app.post("/tambah", (req, res) => {
-      const insertSql = "INSERT INTO user (nama, kelas) VALUES (?, ?)"
-      db.query(insertSql, [req.body.nama, req.body.kelas], (err, result) => {
+    app.post("/tambah", upload.any(), (req, res) => {
+      const insertSql = "INSERT INTO user (nama, univ, nim, mulai, akhir, foto) VALUES (?, ?, ?, ?, ?, ?)"
+      const filename = req.files[0].filename
+      const filepath = `./uploads/${filename}`
+
+      db.query(insertSql, [req.body.nama, req.body.univ, req.body.nim, req.body.mulai, req.body.akhir, filename], (err, result) => {
         if (err) throw err
         res.redirect("/data-mhs")
       })
     })
 
+    // Tambahkan route untuk mengembalikan gambar BLOB
+    app.get("/gambar/:id", (req, res) => {
+      const id = req.params.id
+      const sql = "SELECT foto FROM user WHERE id = ?"
+
+      db.query(sql, id, (err, result) => {
+        if (err) throw err
+        const gambar = result[0].foto
+        const fs = require('fs')
+        const filePath = `./uploads/${gambar}`
+        fs.readFile(filePath, (err, data) => {
+          if (err) throw err
+          res.set("Content-Type", "image/jpeg")
+          res.send(data)
+        })
+      })
+    })
+
     // Update database
-    app.post("/update/:id", (req, res) => {
-      const updateSql = "UPDATE user SET nama = ?, kelas = ? WHERE id = ?"
+    app.post("/update/:id", upload.single('newFilename'), (req, res) => {
+      const updateSql = "UPDATE user SET nama = ?, univ = ?, nim = ?, mulai = ?, akhir = ?, foto = ? WHERE id = ?"
       const data = {
         nama: req.body.nama,
-        kelas: req.body.kelas,
+        univ: req.body.univ,
+        nim: req.body.nim,
+        mulai: req.body.mulai,
+        akhir: req.body.akhir,
       }
       const id = req.params.id
-      db.query(updateSql, [data.nama, data.kelas, id], (err, result) => {
+      const sql = "SELECT foto FROM user WHERE id = ?"
+      db.query(sql, id, (err, result) => {
         if (err) throw err
-        res.redirect("/data-mhs")
+        const userData = result[0]
+        if (req.file) {
+          data.foto = req.file.filename
+          db.query(updateSql, [data.nama, data.univ, data.nim, data.mulai, data.akhir, data.foto, id], (err, result) => {
+            if (err) throw err
+            // Hapus foto lama
+            const fs = require('fs')
+            const filePath = `./uploads/${userData.foto}`
+            fs.unlink(filePath, (err) => {
+              if (err) throw err
+              console.log('Foto lama telah dihapus')
+            })
+            res.redirect("/data-mhs")
+          })
+        } else {
+          db.query("UPDATE user SET nama = ?, univ = ?, nim = ?, mulai = ?, akhir = ? WHERE id = ?", [data.nama, data.univ, data.nim, data.mulai, data.akhir, id], (err, result) => {
+            if (err) throw err
+            res.redirect("/data-mhs")
+          })
+        }
       })
     })
 
@@ -119,12 +175,6 @@ db.connect((err) => {
               if (err) {
                 console.log(err)
               } else if (isMatch) {
-                // console.log("Login successful!")
-                // const sql = "SELECT * FROM user"
-                // db.query(sql, (err, result) => {
-                //   const users = JSON.parse(JSON.stringify(result))
-                //   res.render("main", {users: users, title: "DAFTAR MAHASISWA MAGANG" })
-                // })
                 const sql = "SELECT * FROM akun"
                 db.query(sql, (err, result) => {
                   const Akun = JSON.parse(JSON.stringify(result))
@@ -151,6 +201,12 @@ db.connect((err) => {
         const akun = JSON.parse(JSON.stringify(result))
         res.render("dashboard", { akun: akun })
       })
+    })
+
+    // tambah
+    app.get("/tambah", (req, res) => {
+      if (err) throw err
+      res.render("tambah")
     })
   })
 
